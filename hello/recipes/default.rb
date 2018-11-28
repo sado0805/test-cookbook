81b11f2e77684b6d58b050469fa2b92d71f7aaf4
sado0805 <45408521+sado0805@users.noreply.github.com>
2018-11-29 00:41:57 +0900

    Update default.rb

--- hello/recipes/default.rb
+++ hello/recipes/default.rb
@@ -1,5 +1,5 @@
------------- default.rb start-------------------
+#------------ default.rb start-------------------
 file "/home/ec2-user/hello.txt" do
   action :create
 end
------------- default.rb-------------------------------
+#------------ default.rb-------------------------------
